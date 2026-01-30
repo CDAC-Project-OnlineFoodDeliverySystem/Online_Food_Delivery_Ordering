200717e016f0393c018f3ea7b4b0bf84dd4fed8a
prajwalgaste <gasteprajwal10@gmail.com>
2026-01-30 15:41:54 +0530

    created ApiGateway in ASP .Net

---
+++ backend/ApiGateway/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add YARP
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapReverseProxy();
+
+app.Run();
